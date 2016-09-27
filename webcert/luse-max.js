2dda33e79cc00d9263c4260e1eabd088763b5be3
Jonas Glenne <jonas.glenne@nordicmedtest.se>
2016-09-27 16:46:09 +0200

    Uppdatering för att hantera ICD-10 koder i luse-maxintyg.

--- webcert/luse-max.js
+++ webcert/luse-max.js
@@ -1,14 +1,20 @@
 var today = new Date();
 var todayDateString = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2)  + '-' + ('0' + today.getDate()).slice(-2);
 
+function createEnterEvent() {
+	var e = jQuery.Event("keydown");
+	e.which = 13;
+	return e;
+}
+
 $("input[id*='date_undersokningAvPatienten_']").click();
 $("input[id*='_date_journaluppgifter_']").click();
 $("input[id*='_date_anhorigsBeskrivningAvPatienten_']").click();
 $("input[id*='_date_annatGrundForMU_']").click();
 $("#annatGrundForMUBeskrivning").val('Journal from down below').change();
 $("#form_kannedomOmPatient > div:nth-child(1) > div:nth-child(2) > span:nth-child(1) > span:nth-child(1) > input:nth-child(1)").val(todayDateString).change();
-/*$("input[id*='-toggle']").click();
-$("#underlag-2-datum").val(todayDateString).change();*/
+$("input[id*='-toggle']").click();
+$("#underlag-2-datum").val(todayDateString).change();
 $("#underlagFinnsYes").prop("checked", true);
 $("#underlagFinnsYes").click();
 $("#underlag-0-typ").val('string:LOGOPED').change();
@@ -24,14 +30,40 @@ $("#underlag-2-datum").val(todayDateString).change();
 $("#underlag-2-hamtasFran").val('CentralSjukhuset!!').change();
 $("#sjukdomsforlopp").val("Patienten har haft besvären i olika omgångar och aldrig riktigt fått vara helt frisk, Patienten känner sig trött och orkeslös efter alla års sjukdom. Forsta insjuknandet inföll sig vid 6 års ålder och har sedan dess pågått.");
 $("#sjukdomsforlopp").change();
-$("#diagnoseCode-0").val('S47').change().blur();                                       /*här är jag*/
-$("#diagnoseDescription-0").val("Klämskada på skuldra och överarm").change().blur();
-$("#laggTillDiagnos").click().blur();
-$("#diagnoseCode-1").val('F000').click().blur();
-$("input[id*='diagnoseDescription']").eq(1).val('Demens vid Alzheimers sjukdom med tidig debut').change().blur();
-$("#laggTillDiagnos").click().blur();
-$("#diagnoseCode-2").val('G000').click().blur();
-$("input[id*='diagnoseDescription']").eq(2).val('Meningit orsakad av Haemophilus influenzae').change().blur();
+$("#diagnoseCode-0").val('S47').change();
+
+var firstFound = false;
+var checkForElement1 = setInterval(function() {
+   if ($("li[id*='option-0']").children("a:contains('S47')").length && firstFound === false) {
+			$("li[id*='option-0']").click();
+			firstFound = true;
+   }
+	 if ($("li[id*='option-0']").children("a:contains('S47')").length === 0 && firstFound === true) {
+		 $("#laggTillDiagnos").click().blur();
+		 $("#diagnoseCode-1").val('F000').change();
+		 clearInterval(checkForElement1);
+	 }
+}, 1000);
+
+var secondFound = false;
+var checkForElement2 = setInterval(function() {
+   if ($("li[id*='option-0']").children("a:contains('F000')").length && secondFound === false) {
+			$("li[id*='option-0']").click();
+			secondFound = true;
+   }
+	 if ($("li[id*='option-0']").children("a:contains('F000')").length === 0 && secondFound === true) {
+		 $("#laggTillDiagnos").click().blur();
+		 $("#diagnoseCode-2").val('G000').change();
+		 clearInterval(checkForElement2);
+	 }
+}, 1000);
+
+var checkForElement3 = setInterval(function() {
+   if ($("li[id*='option-0']").children("a:contains('G000')").length) {
+			$("li[id*='option-0']").children("a:contains('G000')").click();
+      clearInterval(checkForElement3);
+   }
+}, 1000);
 $("#diagnosgrund").val("I tidernas begynnelse ställdes diagnosen för patienten på Sahlgrenska").change();
 $("#nyBedomningDiagnosgrundNo").prop("checked", true);
 $("#nyBedomningDiagnosgrundNo").click();
