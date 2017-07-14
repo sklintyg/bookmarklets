cf132ae1ec3ab5204ef339aac182d9bf22c9f9cf
tommy <tommy.johanssson@nordicmedtest.se>
2017-07-14 11:20:35 +0200

    ändrat

--- webcert/smitta-min.js
+++ webcert/smitta-min.js
@@ -1,7 +1,12 @@
 $("#smittskydd").click();
 $("#capacityForWorkActualWorkingHoursPerWeek").val("38,5").change();
 $("#nedsattMed50").click();
-$("#capacityForWorkText").val("Sjukare än vi först trodde.");
+$("#capacityForWorkText").val("Sjukare än vi först trodde.
+Jättemycket text Jättemycket text Jättemycket text Jättemycket textJättemycket text Jättemycket text Jättemycket text 
+Jättemycket text Jättemycket textJättemycket text Jättemycket text Jättemycket text Jättemycket text Jättemycket text
+Jättemycket text Jättemycket text Jättemycket text JättemJättemycket text Jättemycket text Jättemycket text 
+Jättemycket textJättemycket text Jättemycket text Jättemycket text Jättemycket text Jättemycket textJättemycket text 
+Jättemycket text Jättemycket text Jättemycket text Jättemycket textJättemycket text Jättemycket text Jättemycket");
 $("#capacityForWork4").prop("checked", true).trigger("click");
 $("#capacityForWorkForecastText").val("Försök själv att prata med patienten får du se hur lätt det är.").change();
 $("#kontaktFk").click();
