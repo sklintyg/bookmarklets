836779c0aa5205d51727530d93b8a8c13ec850f3
tommy <tommy.johanssson@nordicmedtest.se>
2017-07-14 09:14:44 +0200

    lagt till går ej att bedöma, kontakt FK, övr.uppl.

--- webcert/smitta-min.js
+++ webcert/smitta-min.js
@@ -2,6 +2,13 @@ $('#smittskydd').trigger("click").blur();
 $("#capacityForWorkActualWorkingHoursPerWeek").val("38,5").change();
 $("#nedsattMed50").click();
 $("#capacityForWorkText").val("Sjukare än vi först trodde.");
+$("#capacityForWork4").prop("checked", true).trigger("click");
+$("#capacityForWorkForecastText").val("Försök själv att prata med patienten får du se hur lätt det är.").change();
+$("#kontaktFk").click();
+$("#otherInformation").val("Jättemycket text Jättemycket text Jättemycket text Jättemycket text 
+	Jättemycket text Jättemycket text Jättemycket text Jättemycket text Jättemycket text   
+	Jättemycket text Jättemycket text Jättemycket text Jättemycket text Jättemycket text 
+	Jättemycket text Jättemycket text Jättemycket text Jättem");
 $("#clinicInfoPhone").val("07388888888");
 $("#clinicInfoPhone").change();
 
