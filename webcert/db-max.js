b0a1b9a1a40e42c6f823a56e261cea498b730e31
tommy <tommy.johanssson@nordicmedtest.se>
2017-10-11 08:01:59 +0200

    Dödsbevis klar

--- webcert/db-max.js
+++ webcert/db-max.js
@@ -3,11 +3,16 @@ var todayDateString = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1))
 
 
 $("#identitetStyrkt").val("Körkort").change();
-$("#dodsdatumSakertYes").prop("checked", true).trigger("click");
-$("#form_dodsdatum").val(todayDateString).change();
-$("#form_dodsdatum > div > div:nth-child(2) > span > span > input").val(todayDateString).change();
+$("#dodsdatumSakertNo").prop("checked", true).trigger("click");
+$("#dodsdatum-year").val("1900").change();
+$("#dodsdatum-month").val("01").change();
+$("#dodsdatum-day").val("01").change();
+$("#form_antraffatDodDatum > div > div:nth-child(2) > span > span > input").val(todayDateString).change();
 $("#dodsplatsKommun").val("Hades Kommun").change();
 $("#dodsplatsBoende-SJUKHUS").prop("checked", true).trigger("click");
 $("#barnYes").prop("checked", true).trigger("click");
 $("#explosivImplantatYes").prop("checked", true).trigger("click");
 $("#explosivAvlagsnatNo").prop("checked", true).trigger("click");
+$("#undersokningYttre-UNDERSOKNING_GJORT_KORT_FORE_DODEN").prop("checked", true).trigger("click");
+$("#form_undersokningDatum > div > div:nth-child(2) > span > span > input").val(todayDateString).change();
+$("#polisanmalanYes").prop("checked", true).trigger("click");
