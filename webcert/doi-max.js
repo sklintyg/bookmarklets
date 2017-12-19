6ef17380bf07b116754c178f3eac174706c393e3
tommy <tommy.johanssson@nordicmedtest.se>
2017-12-19 11:40:47 +0100

    nya DOI

--- webcert/doi-max.js
+++ webcert/doi-max.js
@@ -1,49 +1,72 @@
 var today = new Date();
-var todayDateString = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2)  + '-' + ('0' + today.getDate()).slice(-2);
-var ddMarker = "";
+var todayDateString = today.getFullYear() + "-" + ("0" + (today.getMonth() + 1)).slice(-2) + "-" + ("0" + today.getDate()).slice(-2);
 
-$("#identitetStyrkt").val("Anhörig").change();
+$("#identitetStyrkt").val("AnhÃ¶rig").change();
 $("#land").val("Mordor").change();
 $("#dodsdatumSakertNo").prop("checked", true).trigger("click");
-$("#dodsdatum-year").val("2017").change();
-$("#dodsdatum-month").val("00").change();
-$("#dodsdatum-day").val("00").change();
 $("#form_antraffatDodDatum > div > div:nth-child(2) > span > span > input").val(todayDateString).change();
 $("#dodsplatsKommun").val("Vattnadal").change();
 $("#dodsplatsBoende-ORDINART_BOENDE").prop("checked", true).trigger("click");
 $("#barnNo").prop("checked", true).trigger("click");
+
+function clickDropdownBoxYearMonth(index) {
+    if ("year" === index) {
+        $("#dodsdatum-year > div.ui-select-match > span > span.ui-select-match-text.pull-left > span").click();
+    } else if ("month" === index) {
+        $("#dodsdatum-month > div.ui-select-match > span > span.ui-select-match-text.pull-left > span").click();
+    }
+}
+
+function selectInDropdownYearMonth(index, type) {
+    if ("year" === type) {
+        $("#dodsdatum-year > ul > li > div").each(function() {
+            var ddElementId = $(this).attr("id");
+            if (ddElementId !== undefined) {
+                if (parseInt(ddElementId.substr((ddElementId.length - 1), ddElementId.length)) === index) {
+                    $("#" + ddElementId).click();
+                }
+            }
+        });
+    } else if ("month" === type) {
+        $("#dodsdatum-month > ul > li > div").each(function() {
+            var ddElementId = $(this).attr("id");
+            if (ddElementId !== undefined) {
+                if (parseInt(ddElementId.substr((ddElementId.length - 1), ddElementId.length)) === index) {
+                    $("#" + ddElementId).click();
+                }
+            }
+        });
+    }
+
+}
+
+clickDropdownBoxYearMonth("year");
+selectInDropdownYearMonth(2, "year");
+clickDropdownBoxYearMonth("month");
+selectInDropdownYearMonth(5, "month");
+
+var ddMarker = "";
+
 function clickDropdownBox(index, multiOrsak) {
-	index = (index !== undefined) ? index : '';
-	index = ddMarker = (multiOrsak) ? `#orsak-multi-${index}-specifikation` : `#orsak-${index}-specifikation`;
-	if (multiOrsak) {
-		$(`${index} > div.ui-select-match > span`).click();
-	} else {
-		$(`${index} > div.ui-select-match > span > span.ui-select-match-text.pull-left > span`).click();
-	}
+    index = (index !== undefined) ? index : "";
+    index = ddMarker = (multiOrsak) ? "#orsak-multi-" + index + "-specifikation" : "#orsak-" + index + "-specifikation";
+    if (multiOrsak) {
+        $(index + " > div.ui-select-match > span").click();
+    } else {
+        $(index + " > div.ui-select-match > span > span.ui-select-match-text.pull-left > span").click();
+    }
 }
 
 function selectInDropdown(index) {
-	$(`${ddMarker} > ul > li > div`).each(function() {
-		var ddElementId = $(this).attr("id");
-		if (ddElementId !== undefined) {
-			if (parseInt(ddElementId.substr((ddElementId.length - 1), ddElementId.length)) === index) {
-				$(`#${ddElementId}`).click();
-			}
-		}
-	});
+    $(ddMarker + " > ul > li > div").each(function() {
+        var ddElementId = $(this).attr("id");
+        if (ddElementId !== undefined) {
+            if (parseInt(ddElementId.substr((ddElementId.length - 1), ddElementId.length)) === index) {
+                $("#" + ddElementId).click();
+            }
+        }
+    });
 }
-
-function maximizeMultiFields() {
-	for (var i = 0; i <= 7; i++) {
-		$('#form_bidragandeSjukdomar > div > button').click();
-	}
-	setTimeout(function() {
-		console.log("Sleep 500 ms");
-	}, 500);
-
-}
-
-maximizeMultiFields();
 
 clickDropdownBox(undefined, false);
 selectInDropdown(3);
@@ -83,31 +106,13 @@ $("#orsak-2-beskrivning").val("Ytterligare nÃ¥got annat.").change();
 $("#orsak-2-datum").val(todayDateString).change();
 $("#orsak-multi-0-beskrivning").val("Denguefeber").change();
 $("#orsak-multi-0-datum").val(todayDateString).change();
-$("#orsak-multi-1-beskrivning").val("Nageltrång").change();
-$("#orsak-multi-1-datum").val(todayDateString).change();
-$("#orsak-multi-2-beskrivning").val("Kluvna hårtoppar").change();
-$("#orsak-multi-2-datum").val(todayDateString).change();
-$("#orsak-multi-3-beskrivning").val("Tummen mitt i handen").change();
-$("#orsak-multi-3-datum").val(todayDateString).change();
-$("#orsak-multi-4-beskrivning").val("Lyssnat på Dansband").change();
-$("#orsak-multi-4-datum").val(todayDateString).change();
-$("#orsak-multi-5-beskrivning").val("Blå tumme").change();
-$("#orsak-multi-5-datum").val(todayDateString).change();
-$("#orsak-multi-6-beskrivning").val("nåt hemskt").change();
-$("#orsak-multi-6-datum").val(todayDateString).change();
-$("#orsak-multi-7-beskrivning").val("Pest och pina").change();
-$("#orsak-multi-7-datum").val(todayDateString).change();
 $("#operation-JA").prop("checked", true).trigger("click");
-$("#form_operationDatum > div > div:nth-child(2) > span > span > input").val(todayDateString).change();
-$("#operationAnledning").val("Det behövdes").change();
+$("#datepicker_operationDatum").val(todayDateString).change();
+$("#operationAnledning").val("Det behÃ¶vdes").change();
 $("#forgiftningYes").prop("checked", true).trigger("click");
 $("#forgiftningOrsak-OKLART").prop("checked", true).trigger("click");
-$("#form_forgiftningDatum > div > div:nth-child(2) > span > span > input").val(todayDateString).change();
-$("#forgiftningUppkommelse").val(`Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång.`).change();
+$("#datepicker_forgiftningDatum").val(todayDateString).change();
+$("#forgiftningUppkommelse").val("Detta Ã¤r en kort text.").change();
 $("#grunder-UNDERSOKNING_FORE_DODEN").click();
 $("#grunder-UNDERSOKNING_EFTER_DODEN").click();
 $("#grunder-KLINISK_OBDUKTION").click();
