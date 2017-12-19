d544bd74033b7d4fd10ccca57fa45967313fcf2d
tommy <tommy.johanssson@nordicmedtest.se>
2017-12-19 11:09:45 +0100

    från komprimerad

--- webcert/db-max.js
+++ webcert/db-max.js
@@ -1,18 +1,51 @@
 var today = new Date();
-var todayDateString = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2)  + '-' + ('0' + today.getDate()).slice(-2);
+var todayDateString = today.getFullYear() + "-" + ("0" + (today.getMonth() + 1)).slice(-2)  + "-" + ("0" + today.getDate()).slice(-2);
 
-
-$("#identitetStyrkt").val("Körkort").change();
+$("#identitetStyrkt").val("AnhÃ¶rig").change();
+$("#land").val("Mordor").change();
 $("#dodsdatumSakertNo").prop("checked", true).trigger("click");
-$("#dodsdatum-year").val("1900").change();
-$("#dodsdatum-month").val("01").change();
-$("#dodsdatum-day").val("01").change();
 $("#form_antraffatDodDatum > div > div:nth-child(2) > span > span > input").val(todayDateString).change();
-$("#dodsplatsKommun").val("Hades Kommun").change();
-$("#dodsplatsBoende-SJUKHUS").prop("checked", true).trigger("click");
-$("#barnYes").prop("checked", true).trigger("click");
+$("#dodsplatsKommun").val("Vattnadal").change();
+$("#dodsplatsBoende-ORDINART_BOENDE").prop("checked", true).trigger("click");
+$("#barnNo").prop("checked", true).trigger("click");
+
+function clickDropdownBox(index) {
+    if("year" === index) {
+        $("#dodsdatum-year > div.ui-select-match > span > span.ui-select-match-text.pull-left > span").click();
+    } else if("month" === index) {
+		$("#dodsdatum-month > div.ui-select-match > span > span.ui-select-match-text.pull-left > span").click();
+    }
+}
+
+function selectInDropdown(index, type) {
+	if("year" === type) {
+		$("#dodsdatum-year > ul > li > div").each(function() {
+			var ddElementId = $(this).attr("id");
+			if (ddElementId !== undefined) {
+				if (parseInt(ddElementId.substr((ddElementId.length - 1), ddElementId.length)) === index) {
+					$("#" + ddElementId).click();
+				}
+			}
+		});
+	} else if("month" === type) {
+		$("#dodsdatum-month > ul > li > div").each(function() {
+			var ddElementId = $(this).attr("id");
+			if (ddElementId !== undefined) {
+				if (parseInt(ddElementId.substr((ddElementId.length - 1), ddElementId.length)) === index) {
+					$("#" + ddElementId).click();
+				}
+			}
+		});
+	}
+
+}
+
+clickDropdownBox("year");
+selectInDropdown(2, "year");
+clickDropdownBox("month");
+selectInDropdown(5, "month");
+
 $("#explosivImplantatYes").prop("checked", true).trigger("click");
-$("#explosivAvlagsnatNo").prop("checked", true).trigger("click");
-$("#undersokningYttre-UNDERSOKNING_GJORT_KORT_FORE_DODEN").prop("checked", true).trigger("click");
-$("#form_undersokningDatum > div > div:nth-child(2) > span > span > input").val(todayDateString).change();
+$("#explosivAvlagsnatYes").prop("checked", true).trigger("click");
+$("#undersokningYttre-JA").prop("checked", true).trigger("click");
 $("#polisanmalanYes").prop("checked", true).trigger("click");
