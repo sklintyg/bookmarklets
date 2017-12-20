f9f5ce022dc01eeca5d3422856a0bc0c86ab950a
tommy <tommy.johanssson@nordicmedtest.se>
2017-12-20 14:05:54 +0100

    ändrade lisjp-max att fungera i IE, ny fil compressed

--- webcert/lisjp-max.js
+++ webcert/lisjp-max.js
@@ -1,10 +1,10 @@
 var today = new Date();
 
 function createDateString(today, daysToAdd) {
-	if(daysToAdd) {
+	if (daysToAdd) {
 		today.setDate(new Date().getDate() + daysToAdd);
 	}
-	var todayDateString = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2)  + '-' + ('0' + today.getDate()).slice(-2);
+	var todayDateString = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
 	return todayDateString;
 }
 
@@ -26,44 +26,44 @@ $("#sysselsattning-FORALDRALEDIG").click();
 $("#sysselsattning-STUDIER").click();
 $("#arbetsmarknadspolitisktProgram").val("Plantskole rektor").change();
 
-$("#diagnoseCode-0").val('S47').change();
+$("#diagnoseCode-0").val("S47").change();
 
 var firstFound = false;
 var checkForElement1 = setInterval(function() {
-   if ($("li[id*='option-0']").children("a:contains('S47')").length && firstFound === false) {
-			$("li[id*='option-0']").click();
-			firstFound = true;
-   }
-	 if ($("li[id*='option-0']").children("a:contains('S47')").length === 0 && firstFound === true) {
-		 $("#laggTillDiagnos").click().blur();
-		 $("#diagnoseCode-1").val('Y1113').change();
-		 clearInterval(checkForElement1);
-	 }
+	if ($("li[id*='option-0']").children("a:contains('S47')").length && firstFound === false) {
+		$("li[id*='option-0']").click();
+		firstFound = true;
+	}
+	if ($("li[id*='option-0']").children("a:contains('S47')").length === 0 && firstFound === true) {
+		$("#laggTillDiagnos").click().blur();
+		$("#diagnoseCode-1").val('Y1113').change();
+		clearInterval(checkForElement1);
+	}
 }, 1000);
 
 var secondFound = false;
 var checkForElement2 = setInterval(function() {
-   if ($("li[id*='option-0']").children("a:contains('Y1113')").length && secondFound === false) {
-			$("li[id*='option-0']").click();
-			secondFound = true;
-   }
-	 if ($("li[id*='option-0']").children("a:contains('Y1113')").length === 0 && secondFound === true) {
-		 $("#laggTillDiagnos").click().blur();
-		 $("#diagnoseCode-2").val('M659B').change();
-		 clearInterval(checkForElement2);
-	 }
+	if ($("li[id*='option-0']").children("a:contains('Y1113')").length && secondFound === false) {
+		$("li[id*='option-0']").click();
+		secondFound = true;
+	}
+	if ($("li[id*='option-0']").children("a:contains('Y1113')").length === 0 && secondFound === true) {
+		$("#laggTillDiagnos").click().blur();
+		$("#diagnoseCode-2").val('M659B').change();
+		clearInterval(checkForElement2);
+	}
 }, 1000);
 
 var checkForElement3 = setInterval(function() {
-   if ($("li[id*='option-0']").children("a:contains('M659B')").length) {
-			$("li[id*='option-0']").children("a:contains('M659B')").click();
-      clearInterval(checkForElement3);
-   }
+	if ($("li[id*='option-0']").children("a:contains('M659B')").length) {
+		$("li[id*='option-0']").children("a:contains('M659B')").click();
+		clearInterval(checkForElement3);
+	}
 }, 1000);
 $("#funktionsnedsattning").val("Kan inte plocka blommor.").change();
-$("#aktivitetsbegransning").val("Se föregående.").change();
-$("#pagaendeBehandling").val("Smörjer med diverse krämer.").change();
-$("#planeradBehandling").val("Mer krämer.").change();
+$("#aktivitetsbegransning").val("Se fÃ¶regÃ¥ende.").change();
+$("#pagaendeBehandling").val("SmÃ¶rjer med diverse krÃ¤mer.").change();
+$("#planeradBehandling").val("Mer krÃ¤mer.").change();
 $("input[id*='_sjukskrivningar_sjukskrivningar_']").val("35.5").change();
 $("#sjukskrivningar-EN_FJARDEDEL-from").val(createDateString(today)).change();
 $("#sjukskrivningar-EN_FJARDEDEL-tom").val(createDateString(today, 5)).change();
@@ -73,8 +73,8 @@ $("#sjukskrivningar-TRE_FJARDEDEL-from").val(createDateString(today, 12)).change
 $("#sjukskrivningar-TRE_FJARDEDEL-tom").val(createDateString(today, 17)).change();
 $("#sjukskrivningar-HELT_NEDSATT-from").val(createDateString(today, 18)).change();
 $("#sjukskrivningar-HELT_NEDSATT-tom").val(createDateString(today, 366)).change();
-$("#motiveringTillForLangSjukskrivning").val("Varför inte?").change();
-$("#forsakringsmedicinsktBeslutsstod").val("Det krävdes mer kräm.").change();
+$("#motiveringTillForLangSjukskrivning").val("VarfÃ¶r inte?").change();
+$("#forsakringsmedicinsktBeslutsstod").val("Det krÃ¤vdes mer krÃ¤m.").change();
 $("#arbetstidsforlaggningYes").prop("checked", true).trigger("click");
 $("#arbetstidsforlaggningMotivering").val("HejHejHej").change();
 $("input[id*='_arbetsresor_0']").click();
@@ -82,9 +82,9 @@ $("#checkbox").prop("checked", true);
 $("input[id*='_arbetsresor_4']").click();
 $("input[id*='-ATER_X_ANTAL_DGR']").prop("checked", true).trigger("click");
 $("#prognosDagarTillArbete-1-typ > div.ui-select-match > span > span.ui-select-match-text.pull-left > span").click();
-$("[id$=-3]").click();  
+$("[id$=-3]").click();
 $("#arbetslivsinriktadeAtgarder-ARBETSTRANING").click();
-$("#arbetslivsinriktadeAtgarderBeskrivning").val("Träna på att arbeta.").change();
+$("#arbetslivsinriktadeAtgarderBeskrivning").val("TrÃ¤na pÃ¥ att arbeta.").change();
 $("#arbetslivsinriktadeAtgarder-ARBETSANPASSNING").click();
 $("#arbetslivsinriktadeAtgarder-SOKA_NYTT_ARBETE").click();
 $("#arbetslivsinriktadeAtgarder-BESOK_ARBETSPLATS").click();
@@ -94,45 +94,7 @@ $("#arbetslivsinriktadeAtgarder-KONFLIKTHANTERING").click();
 $("#arbetslivsinriktadeAtgarder-KONTAKT_FHV").click();
 $("#arbetslivsinriktadeAtgarder-OMFORDELNING").click();
 $("#arbetslivsinriktadeAtgarder-OVRIGA_ATGARDER").click();
-$("#arbetslivsinriktadeAtgarderAktuelltBeskrivning").val("Patienten behöver hjälp med allt.").change();
-$("#ovrigt").val(`Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text.
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text.
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text.
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text.Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. Detta blir en lång text. 
-Detta blir en.`).change();
+$("#arbetslivsinriktadeAtgarderAktuelltBeskrivning").val("Patienten behÃ¶ver hjÃ¤lp med allt.").change();
+$("#ovrigt").val('Detta blir en kort text.').change();
 $("input[id*='_checkbox-inline_kontaktMedFk_0']").click();
-$("#anledningTillKontakt").val("Därför.").change();
+$("#anledningTillKontakt").val("DÃ¤rfÃ¶r.").change();
