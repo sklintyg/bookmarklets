ec9a63f745d8ff1563491b8cfa2b180e7e1671df
Markus Lundin <m.lundin.1986@gmail.com>
2018-02-21 10:44:04 +0100

    Uppdatera DOI bookmarklet för att passa Webcert 6.0

--- webcert/doi-max.js
+++ webcert/doi-max.js
@@ -1,49 +1,14 @@
 var today = new Date();
 var todayDateString = today.getFullYear() + "-" + ("0" + (today.getMonth() + 1)).slice(-2) + "-" + ("0" + today.getDate()).slice(-2);
 
-$("#identitetStyrkt").val("AnhÃ¶rig").change();
+$("#identitetStyrkt").val("Anhörig").change();
 $("#land").val("Mordor").change();
 $("#dodsdatumSakertNo").prop("checked", true).trigger("click");
-$("#form_antraffatDodDatum > div > div:nth-child(2) > span > span > input").val(todayDateString).change();
+$("#datepicker_antraffatDodDatum").val(todayDateString).change();
 $("#dodsplatsKommun").val("Vattnadal").change();
 $("#dodsplatsBoende-ORDINART_BOENDE").prop("checked", true).trigger("click");
 $("#barnNo").prop("checked", true).trigger("click");
 
-function clickDropdownBoxYearMonth(index) {
-    if ("year" === index) {
-        $("#dodsdatum-year > div.ui-select-match > span > span.ui-select-match-text.pull-left > span").click();
-    } else if ("month" === index) {
-        $("#dodsdatum-month > div.ui-select-match > span > span.ui-select-match-text.pull-left > span").click();
-    }
-}
-
-function selectInDropdownYearMonth(index, type) {
-    if ("year" === type) {
-        $("#dodsdatum-year > ul > li > div").each(function() {
-            var ddElementId = $(this).attr("id");
-            if (ddElementId !== undefined) {
-                if (parseInt(ddElementId.substr((ddElementId.length - 1), ddElementId.length)) === index) {
-                    $("#" + ddElementId).click();
-                }
-            }
-        });
-    } else if ("month" === type) {
-        $("#dodsdatum-month > ul > li > div").each(function() {
-            var ddElementId = $(this).attr("id");
-            if (ddElementId !== undefined) {
-                if (parseInt(ddElementId.substr((ddElementId.length - 1), ddElementId.length)) === index) {
-                    $("#" + ddElementId).click();
-                }
-            }
-        });
-    }
-
-}
-
-clickDropdownBoxYearMonth("year");
-selectInDropdownYearMonth(2, "year");
-clickDropdownBoxYearMonth("month");
-selectInDropdownYearMonth(5, "month");
 
 var ddMarker = "";
 
