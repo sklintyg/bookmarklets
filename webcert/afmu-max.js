e9c413cb42ba066c574dbca818ab7fe2ddbe5427
Tommy Johansson <tommy.johansson@nordicmedtest.se>
2018-10-26 13:09:42 +0200

    Gjort en bookmarklet för AFMU.

---
+++ webcert/afmu-max.js
@@ -0,0 +1,9 @@
+$("#harFunktionsnedsattningYes").prop("checked", true).trigger("click");
+$("#funktionsnedsattning").val("En text om ingenting.");
+$("#harAktivitetsbegransningYes").prop("checked", true).trigger("click");
+$("#aktivitetsbegransning").val("Additional police were already on campus because a student had notified a teacher of a rumored threat to students.");
+$("#harUtredningBehandlingYes").prop("checked", true).trigger("click");
+$("#utredningBehandling").val("The girls also had a goblet from which to drink blood, authorities said.");
+$("#harArbetetsPaverkanYes").prop("checked", true).trigger("click");
+$("#arbetetsPaverkan").val("When Ross Capicchioni was 17, he was lured into a bad neighborhood in Detroit's notorious east side, shot three times and left for dead. If Capicchioni had perished, his murder likely wouldn't have filtered all the way up to us, it would have read, just another kid gunned down in a bad part of a big city.");
+$("#ovrigt").val("Övrigt övrigt övrigt");
