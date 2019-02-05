69623145b8787afaf42963a40ea4975382ae6898
Tommy Johansson <tommy.johansson@nordicmedtest.se>
2019-02-05 13:27:50 +0100

    tagit bort ovanliga tecken

--- webcert/db-max.js
+++ webcert/db-max.js
@@ -1,10 +1,9 @@
 var today = new Date();
 var todayDateString = today.getFullYear() + "-" + ("0" + (today.getMonth() + 1)).slice(-2)  + "-" + ("0" + today.getDate()).slice(-2);
-$("#identitetStyrkt").val("!#$%&*+,-./0123456789:<=>").change();
-$("#land").val("Mordor").change();
+$("#identitetStyrkt").val("Lite text.").change();
 $("#dodsdatumSakertNo").prop("checked", true).trigger("click");
 $("#datepicker_antraffatDodDatum").val(todayDateString).change();
-$("#dodsplatsKommun").val("Vattnadal").change();
+$("#dodsplatsKommun").val("*3*5*7*9*12*15*18*21*24*27*30*33*36*39*42*45*48*51*54*57*60*63*66*69*72*75*78*81*84*87*90*93*96*100*").change();
 $("#dodsdatum-year-selected-item-label").click();
 $("#dodsdatum-year-2018").click();
 $("#dodsdatum-month-selected-item-label").click();
