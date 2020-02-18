4d73dea892e6ab5ba0791beea5a710f852cb35fa
Anders Johansson <anders.johansson@nordicmedtest.se>
2020-02-18 14:00:28 +0100

    rensat bland filer

--- webcert/afmu-max.js
+++
@@ -1,9 +0,0 @@
-$("#harFunktionsnedsattningYes").prop("checked", true).trigger("click");
-$("#funktionsnedsattning").val("En text om ingenting.").change();
-$("#harAktivitetsbegransningYes").prop("checked", true).trigger("click");
-$("#aktivitetsbegransning").val("Additional police were already on campus because a student had notified a teacher of a rumored threat to students.").change();
-$("#harUtredningBehandlingYes").prop("checked", true).trigger("click");
-$("#utredningBehandling").val("The girls also had a goblet from which to drink blood, authorities said.").change();
-$("#harArbetetsPaverkanYes").prop("checked", true).trigger("click");
-$("#arbetetsPaverkan").val('"!#$%&*+,-./0123456789:<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^)_`abcdefghijklmnopqrstuvwxyz{|}~€‚ƒ„…†‡ˆ‰Š‹ŒŽ‘’“”•–—˜™šžŸ ¡¢£¤¥¦§¨©ª«¬­¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ)').change();
-$("#ovrigt").val("Övrigt övrigt övrigt");
