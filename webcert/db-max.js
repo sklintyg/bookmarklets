6f695ce63735e336141baad71c48a16aee65cf96
Anders Johansson <anders.johansson@nordicmedtest.se>
2021-01-26 15:34:58 +0100

    uppdaterade db bookmarklet

--- webcert/db-max.js
+++ webcert/db-max.js
@@ -3,11 +3,11 @@ var todayDateString = today.getFullYear() + "-" + ("0" + (today.getMonth() + 1))
 $("#identitetStyrkt").val("Lite text.").change();
 $("#dodsdatumSakertNo").prop("checked", true).trigger("click");
 $("#datepicker_antraffatDodDatum").val(todayDateString).change();
-$("#dodsplatsKommun").val("Karlstad2").change();
+$("#dodsplatsKommun").val("Karlstad").change();
 $("#dodsdatum-year-selected-item-label").click();
 $("#dodsdatum-year-2020").click();
 $("#dodsdatum-month-selected-item-label").click();
-$("#dodsdatum-month-05").click();
+$("#dodsdatum-month-06").click();
 $("#dodsplatsBoende-ORDINART_BOENDE").prop("checked", true).trigger("click");
 $("#barnNo").prop("checked", true).trigger("click");
 $("#explosivImplantatYes").prop("checked", true).trigger("click");
