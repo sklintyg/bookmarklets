26fe3daec6da0ce8c10f36daee5ff9365918363c
Kristina Gullman <kristina.gullman@nordicmedtest.se>
2021-10-13 17:20:00 +0200

    INTYGFV-00: Added som bookmarklet to test

---
+++ webcert/setNative.js
@@ -0,0 +1,13 @@
+function setNativeValue(element, value) {
+   // console.log("Hittar scriptet!");
+     // const valueSetter = Object.getOwnPropertyDescriptor(element, 'value').set;
+     // const prototype = Object.getPrototypeOf(element);
+    //  const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value').set;
+    alert(value);
+    
+    //  if (valueSetter && valueSetter !== prototypeValueSetter) {
+     //     prototypeValueSetter.call(element, value);
+    //  } else {
+    //    valueSetter.call(element, value);
+     // }
+}
