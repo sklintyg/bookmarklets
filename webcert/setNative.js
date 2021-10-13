d30d7377591473b1c8e1d8fa722ffa2a1075e79f
Kristina Gullman <kristina.gullman@nordicmedtest.se>
2021-10-13 19:06:58 +0200

    INTYGFV-00:Working with bookmarklet

--- webcert/setNative.js
+++ webcert/setNative.js
@@ -4,6 +4,8 @@ function setNativeValue(element, value) {
      // const prototype = Object.getPrototypeOf(element);
     //  const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value').set;
     alert(value);
+    element.value = value;
+    element.dispatchEvent(new Event('change',{bubbles:true}));
     
     //  if (valueSetter && valueSetter !== prototypeValueSetter) {
      //     prototypeValueSetter.call(element, value);
